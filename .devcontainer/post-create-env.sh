58fc14cfd782257c81773f5dbacd011077808507
ObjectOops <92550885+ObjectOops@users.noreply.github.com>
2023-08-30 00:02:12 +0000

    Initial test.

--- .devcontainer/post-create-env.sh
+++ .devcontainer/post-create-env.sh
@@ -1,3 +1,4 @@
 # echo 'export ANDROID_HOME="/opt/android-sdk/"' >> $HOME/.bashrc
 # echo 'export PATH="${PATH}:${ANDROID_HOME}tools/:${ANDROID_HOME}platform-tools/"' >> $HOME/.bashrc
 git clone --depth 1 https://github.com/FIRST-Tech-Challenge/FtcRobotController.git "/workspaces/$(ls /workspaces | head -n 1)/FtcRobotController/"
+rm -rf FtcRobotController/.git
